29496ea517babcaa1fd737c46a543888e96721aa
Leslie-Du <1187642896@qq.com>
2017-09-27 21:58:02 +0800

    网络编程
    一个简单的聊天程序（前篇）

--- 网络编程/src/indi/udp/UDPSendDemo2.java
+++ 网络编程/src/indi/udp/UDPSendDemo2.java
@@ -1,13 +1,13 @@
 package indi.udp;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.SocketException;
-import java.net.UnknownHostException;
 
-public class UDPSendDemo {
+public class UDPSendDemo2 {
 
 	public static void main(String[] args) throws IOException {
 
@@ -26,15 +26,24 @@ public class UDPSendDemo {
 		//1.建立udp的socket服务。使用DatagramSocket对象。
 		DatagramSocket ds = new DatagramSocket(8888);
 		
-		//2.将要发送的数据封装到数据包中。
-		String str = "udp传输演示！来了来了！！";
-			//使用DatagramPacket将数据封装到该对象包中。
-		byte[] buf = str.getBytes();
-		DatagramPacket dp = 
-				new DatagramPacket(buf, buf.length, InetAddress.getByName("127.0.0.1"), 10000);
+//		String str = "udp传输演示！来了来了！！";
 		
-		//3.通过udp的socket服务将数据包发送出去。使用send方法。
-		ds.send(dp);
+		BufferedReader bufr = new BufferedReader(new InputStreamReader(System.in));
+		String line = null;
+		
+		while((line=bufr.readLine())!=null){
+			byte[] buf = line.getBytes();
+			
+			DatagramPacket dp = 
+					new DatagramPacket(buf, buf.length, InetAddress.getByName("127.0.0.1"), 10000);
+			ds.send(dp);
+			
+			if("over".equals(line))
+				break;
+		}
+		
+		
+		
 		
 		//4.关闭socket服务。
 		ds.close();
